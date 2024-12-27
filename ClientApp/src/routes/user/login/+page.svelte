beb40558d11e619ee3c5656389303029481abae4
Adam <adamjpolak@gmail.com>
2024-12-26 19:54:55 -0600

    make login and createuser pages forms

--- ClientApp/src/routes/user/login/+page.svelte
+++ ClientApp/src/routes/user/login/+page.svelte
@@ -1,11 +1,28 @@
 <script lang="ts">
+    import { ValidPassword, validPassword, ValidUser, validUsername } from "$lib/validateText";
+
     let username = $state("");
     let usernameError = $state("");
 
     let password = $state("");
     let passwordError = $state("");
 
+    let generalError = $state("");
+
     function tryLogin() {
+
+        const validateUser = validUsername(username);
+        if(validateUser != ValidUser.OK) {
+            usernameError = "Invalid username";
+            return;
+        } else usernameError = "";
+
+        const validatePassword = validPassword(password);
+        if(validatePassword != ValidPassword.OK) {
+            passwordError = "Invalid password";
+        } else passwordError = "";
+
+        // try login
         console.log(username);
         console.log(password);
     }
@@ -13,38 +30,43 @@
 
 <div class="bg-gray-100 h-screen flex items-center justify-center">
     <div class="bg-white shadow-lg rounded-lg p-8 max-w-sm w-full">
-        <h2 class="text-2xl font-bold text-gray-800 text-center mb-4">Login</h2>
-        <div class="mb-4">
-            <label for="username" class="block text-gray-700 text-sm font-medium mb-2">Username</label>
-            <input 
-                type="text" id="username" placeholder="Enter your username" 
-                class="w-full px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-400"
-                bind:value={username}
-            >
-            {#if usernameError.length != 0}
-                <p class="text-red-400 ml-4">*{usernameError}</p>
+        <form onsubmit={tryLogin}>
+            <h2 class="text-2xl font-bold text-gray-800 text-center mb-4">Login</h2>
+            {#if generalError.length != 0}
+                <h4 class="text-center mb-3 text-md text-red-400">*{generalError}</h4>
             {/if}
-        </div>
-        <div class="mb-4">
-            <label for="password" class="block text-gray-700 text-sm font-medium mb-2">Password</label>
-            <input 
-                type="password" 
-                id="password" 
-                placeholder="Enter your password" 
-                class="w-full px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-400"
-                bind:value={password}
-            >
-            {#if passwordError.length != 0}
-                <p class="text-red-400 ml-4">*{usernameError}</p>
-            {/if}
-        </div>
-        <button
-            onclick={tryLogin}
-            class="w-full bg-blue-600 text-white py-2 px-4 rounded-lg hover:bg-blue-700 transition duration-300">
-            Login
-        </button>
-      <p class="text-sm text-gray-500 text-center mt-4">
-        Don't have an account? <a href="/user/createuser" class="text-blue-600 hover:underline">Sign up</a>
-      </p>
+            <div class="mb-4">
+                <label for="username" class="block text-gray-700 text-sm font-medium mb-2">Username</label>
+                <input 
+                    type="text" id="username" placeholder="Enter your username" 
+                    class="w-full px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-400"
+                    bind:value={username}
+                >
+                {#if usernameError.length != 0}
+                    <p class="text-red-400 ml-4">*{usernameError}</p>
+                {/if}
+            </div>
+            <div class="mb-4">
+                <label for="password" class="block text-gray-700 text-sm font-medium mb-2">Password</label>
+                <input 
+                    type="password" 
+                    id="password" 
+                    placeholder="Enter your password" 
+                    class="w-full px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-400"
+                    bind:value={password}
+                >
+                {#if passwordError.length != 0}
+                    <p class="text-red-400 ml-4">*{usernameError}</p>
+                {/if}
+            </div>
+            <button
+                type="submit"
+                class="w-full bg-blue-600 text-white py-2 px-4 rounded-lg hover:bg-blue-700 transition duration-300">
+                Login
+            </button>
+        <p class="text-sm text-gray-500 text-center mt-4">
+            Don't have an account? <a href="/user/createuser" class="text-blue-600 hover:underline">Sign up</a>
+        </p>
+        </form>
     </div>
 </div>
