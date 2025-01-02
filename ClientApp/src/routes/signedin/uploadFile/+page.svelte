165f3fa593b993bf505f221a94ba729cf0d2e179
Adam <adamjpolak@gmail.com>
2025-01-02 16:24:42 -0600

    add form on upload page

--- ClientApp/src/routes/signedin/uploadFile/+page.svelte
+++ ClientApp/src/routes/signedin/uploadFile/+page.svelte
@@ -20,47 +20,49 @@
       shadow bg-white"
     >
       <h1 class="text-2xl mb-8">Upload File</h1>
-      <div class="mb-10">
-        <label for="file" class="file-upload">
-          <div class="flex gap-3">
-            <div
-              style="cursor: pointer;"
-              class="shadow py-1 px-3 bg-blue-400 text-white rounded-md"
+      <form method="POST" action="">
+        <div class="mb-10">
+          <label for="file" class="file-upload">
+            <div class="flex gap-3">
+              <div
+                style="cursor: pointer;"
+                class="shadow py-1 px-3 bg-blue-400 text-white rounded-md"
+              >
+                Choose File
+              </div>
+              <div class="mt-1">
+                {#if fileName}
+                  <p>{fileName}</p>
+                {:else}
+                  <p>no file selected...</p>
+                {/if}
+              </div>
+            </div>
+          </label>
+          <input
+            class="inline-block py-1 px-3 shadow bg-blue-400 text-white rounded-md mb-10"
+            type="file"
+            id="file"
+            name="file"
+          />
+        </div>
+        <div class="mt-3 flex justify-between w-[100%]">
+          <div>
+            <a
+              href="/signedin"
+              class="bg-red-400 text-white text-l px-5 py-2 rounded-md hover:bg-red-500 inline-block"
+              >Cancel</a
             >
-              Choose File
-            </div>
-            <div class="mt-1">
-              {#if fileName}
-                <p>{fileName}</p>
-              {:else}
-                <p>no file selected...</p>
-              {/if}
-            </div>
           </div>
-        </label>
-        <input
-          class="inline-block py-1 px-3 shadow bg-blue-400 text-white rounded-md mb-10"
-          type="file"
-          id="file"
-          name="file"
-        />
-      </div>
-      <div class="mt-3 flex justify-between w-[100%]">
-        <div>
-          <a
-            href="/signedin"
-            class="bg-red-400 text-white text-l px-5 py-2 rounded-md hover:bg-red-500 inline-block"
-            >Cancel</a
-          >
+          <div>
+            <button
+              type="submit"
+              class="bg-blue-500 text-white text-l px-5 py-2 rounded-md hover:bg-blue-600"
+              >Upload</button
+            >
+          </div>
         </div>
-        <div>
-          <button
-            type="submit"
-            class="bg-blue-500 text-white text-l px-5 py-2 rounded-md hover:bg-blue-600"
-            >Upload</button
-          >
-        </div>
-      </div>
+      </form>
     </div>
   </main>
 </div>
