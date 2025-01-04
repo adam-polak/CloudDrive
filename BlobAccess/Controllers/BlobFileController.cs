0b8179412701c2a338aeca1dd53182d0c0aae722
Adam <adamjpolak@gmail.com>
2025-01-03 19:57:51 -0600

    start on blob controller

--- BlobAccess/Controllers/BlobFileController.cs
+++ BlobAccess/Controllers/BlobFileController.cs
@@ -1,10 +1,14 @@
+using Azure.Storage.Blobs;
+using CloudDrive.BlobAccess.Lib;
+
 namespace CloudDrive.BlobAccess.Controllers;
 
 public class BlobFileController
 {
+    private BlobContainerClient _blobClient;
     
     public BlobFileController()
     {
-
+        _blobClient = BlobConnectionHandler.CreateBlobConnection();
     }
 }
--- BlobAccess/Lib/BlobConnectionHandler.cs
+++ BlobAccess/Lib/BlobConnectionHandler.cs
@@ -1,9 +1,17 @@
+using Azure.Storage.Blobs;
+using CloudDrive.Lib;
+
 namespace CloudDrive.BlobAccess.Lib;
 
 public static class BlobConnectionHandler
 {
-    public static void CreateBlobConnection()
+    public static BlobContainerClient CreateBlobConnection()
     {
-        // TODO
+        string? connectionString = Environment.GetEnvironmentVariable("BlobConnectionString");
+        if(connectionString == null) {
+            return new BlobContainerClient(JsonInfo.GetJsonDevVariable("BlobConnectionString"), JsonInfo.GetJsonDevVariable("BlobContainerName"));
+        } else {
+            return new BlobContainerClient(connectionString, Environment.GetEnvironmentVariable("BlobContainerName"));
+        }
     }
 }
