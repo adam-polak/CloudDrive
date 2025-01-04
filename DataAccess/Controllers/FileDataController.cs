8b36803364bce2e2c73199d524447419d2e9502b
Adam <adamjpolak@gmail.com>
2025-01-03 19:46:08 -0600

    write filedatacontroller methods

--- DataAccess/Controllers/FileDataController.cs
+++ DataAccess/Controllers/FileDataController.cs
@@ -1,6 +1,7 @@
 using System.Data.Common;
 using CloudDrive.DataAccess.Lib;
 using CloudDrive.DataAccess.Models;
+using Dapper;
 
 namespace CloudDrive.DataAccess.Controllers;
 
@@ -13,36 +14,74 @@ public class FileDataController
         _connection = DbConnectionHandler.CreateDbConnection();
     }
 
+    private void DoCommand(string sql, object[] parameters)
+    {
+        _connection.Open();
+        _connection.Execute(sql, parameters);
+        _connection.Close();
+    }
+
+    private List<T> DoQuery<T>(string sql, object parameters)
+    {
+        _connection.Open();
+        List<T> list = _connection.Query<T>(sql, parameters).AsList();
+        _connection.Close();
+        return list;
+    }
+
     public void CreateFile(int folderId, string fileName)
     {
-        // TOOD
+        string sql = "INSERT INTO file_table"
+                    + " (folderid, file_name)"
+                    + " VALUES ( @folderid, @file_name);";
+        object[] parameters = { new { folderid = folderId, file_name = fileName } };
+
+       DoCommand(sql, parameters); 
     }
 
     public void DeleteFile(int folderId, int fileId)
     {
-        // TODO
+        string sql = "DELETE FROM file_table"
+                    + " WHERE folderid = @folderid AND id = @id;";
+        object[] parameters = { new { folderid = folderId, id = fileId } };
+
+        DoCommand(sql, parameters);
     }
 
     public void DeleteFilesInFolder(int folderId)
     {
-        // TOOD
+        string sql = "DELETE FROM file_table"
+                    + " WHERE folderid = @folderid;";
+        object[] parameters = { new { folderid = folderId } };
+
+        DoCommand(sql, parameters);    
     }
 
     public void UpdateFile(int fileId, int folderId, string name)
     {
-        // TODO
+        string sql = "UPDATE file_table"
+                    + " SET folderid = @folderid, file_name = @file_name"
+                    + " WHERE id = @id;";
+        object[] parameters = { new { id = fileId, folderid = folderId, file_name = name } };
+
+        DoCommand(sql, parameters);
     }
 
     public FileModel GetFile(int folderId, int fileId)
     {
-        // TODO
+        string sql = "SELECT * FROM file_table"
+                    + " WHERE folderid = @folderid AND id = @id;";
+        
+        object parameters = new { folderid = folderId, id = fileId };
 
-        return null;
+        return DoQuery<FileModel>(sql, parameters).First();
     }
 
     public List<FileModel> GetFilesInFolder(int folderId)
     {
-        // TODO
-        return [];
+        string sql = "SELECT * FROM file_table"
+                    + " WHERE folderid = @folderid;";
+
+        return DoQuery<FileModel>(sql, new { folderid = folderId });
     }
 }
